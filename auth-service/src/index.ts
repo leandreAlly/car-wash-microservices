7dcbae98f68a12e5f78793a1a12c0299066cdc28
leandre <tuyambazeleandre@gmail.com>
2024-01-30 21:24:18 +0200

    Set up nginx and fix catch all route problem

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -5,8 +5,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const startServer = async () => {
-  const PORT: string | number = process.env.PORT || 8080;
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
@@ -17,8 +15,8 @@ const startServer = async () => {
   } catch (err) {
     console.error(err);
   }
-  app.listen(PORT, () => {
-    console.log(`Listening on Port: ${PORT}.......`);
+  app.listen(3000, () => {
+    console.log(`Listening on Port: 3000.......`);
   });
 };
 
