6c3b09b8f8584475f37df2dde397ea5e222a39ca
leandre <tuyambazeleandre@gmail.com>
2024-02-06 23:44:54 +0200

    ft(redis):Set up redis instance

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,13 +1,27 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import dotenv from 'dotenv';
+import { createClient } from 'redis';
 
 dotenv.config();
+
+const REDISPORT = 6379;
+
+const client = createClient({
+  url: 'redis://redis-service:6379',
+});
 
 const startServer = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  if (!process.env.REDIS_HOST) {
+    throw new Error('REDIS_HOST must be defined');
+  }
+
+  client.on('error', (err) => console.error(err));
+  client.on('connect', () => console.log('connected to redis'));
+  await client.connect();
 
   try {
     await mongoose.connect(process.env.MONGO_URI);
