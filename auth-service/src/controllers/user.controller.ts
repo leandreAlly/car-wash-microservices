b1a4f9c6182dd800576da41d0adda14eca7d3955
leandre <tuyambazeleandre@gmail.com>
2024-02-07 19:50:25 +0200

    ft(verify-email): Verify email with otp

--- auth-service/src/controllers/user.controller.ts
+++ auth-service/src/controllers/user.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 import { User } from '../models/user';
 import { UserAttrs } from '../utils/type';
-import { ConflictRequestError, asyncWrapper } from 'error-ease';
+import {
+  BadRequestError,
+  ConflictRequestError,
+  asyncWrapper,
+} from 'error-ease';
 import { PasswordUtil } from '../services/password';
 import otpGenerator from 'otp-generator';
 import { client } from '../index';
@@ -25,7 +29,10 @@ const registerUser = asyncWrapper(
 
     const user = User.build({ email, name, password: hashedPassword });
 
-    client.set(email, otp);
+    client.set(email, otp, {
+      EX: 60 * 5, // 5 minutes
+      NX: true, // only set the key if it does not already exist
+    });
 
     await user.save();
 
@@ -35,4 +42,24 @@ const registerUser = asyncWrapper(
   }
 );
 
-export { registerUser };
+const verifyOtp = asyncWrapper(
+  async (
+    req: Request<{}, {}, { email: string; otp: string }>,
+    res: Response
+  ) => {
+    const { email, otp } = req.body;
+
+    const storedOTP = await client.get(email);
+
+    if (storedOTP !== otp) {
+      throw new BadRequestError('Invalid OTP');
+    }
+
+    client.del(email);
+    await User.findOneAndUpdate({ email }, { isVerified: true });
+
+    res.status(200).json({ message: 'User verified successfully' });
+  }
+);
+
+export { registerUser, verifyOtp };
--- auth-service/src/routes/api/user.routes.ts
+++ auth-service/src/routes/api/user.routes.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { registerUser } from '../../controllers/user.controller';
+import { registerUser, verifyOtp } from '../../controllers/user.controller';
 import { validate } from '../../services/validation';
 import { userRegisterSchema } from '../../utils/validationSchemas';
 
 const router = express.Router();
 
 router.post('/register', validate(userRegisterSchema), registerUser);
-
+router.post('/verify-email', verifyOtp);
 export { router as userRoutes };
