1a7541752248c5ce44279a495212e2b29e3cf2fc
leandre <tuyambazeleandre@gmail.com>
2024-02-10 20:42:47 +0200

    ft(user-login): Add validation to user inputs

--- auth-service/src/routes/api/user.routes.ts
+++ auth-service/src/routes/api/user.routes.ts
@@ -8,13 +8,19 @@ import { validate } from '../../services/validation';
 import {
   userOtpSchema,
   userRegisterSchema,
+  userSignInSchema,
 } from '../../utils/validationSchemas';
 import { checkIfUserIsVerified } from '../../middleware/user.middleware';
 
 const router = express.Router();
 
 router.post('/register', validate(userRegisterSchema), registerUser);
-router.post('/login', checkIfUserIsVerified, signInUser);
 router.post('/verify-email', validate(userOtpSchema), verifyOtp);
+router.post(
+  '/login',
+  validate(userSignInSchema),
+  checkIfUserIsVerified,
+  signInUser
+);
 
 export { router as userRoutes };
--- auth-service/src/utils/validationSchemas.ts
+++ auth-service/src/utils/validationSchemas.ts
@@ -31,3 +31,17 @@ export const userOtpSchema = z.object({
       .max(4, 'OTP must be 4 characters'),
   }),
 });
+
+export const userSignInSchema = z.object({
+  body: z.object({
+    email: z
+      .string({
+        required_error: 'Email is required',
+      })
+      .email('Not a valid email'),
+    password: z
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .max(64, 'Password must be at most 64 characters'),
+  }),
+});
